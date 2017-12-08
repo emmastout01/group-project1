3e0e205836e4157a4f7975a7d35513e8bddec618
Marta Jopp <martapeterson@gmail.com>
2017-12-08 16:01:33 -0600

    adjusted date selector to select dates

--- server/public/scripts/controllers/popupTest.controller.js
+++ server/public/scripts/controllers/popupTest.controller.js
@@ -1,6 +1,14 @@
-myApp.controller('PopupTestController', function ($mdDialog, UserService) {
+myApp.controller('PopupTestController', function ($mdDialog, $scope, UserService) {
 
     vm = this;
+    // vm.userService = UserService;
+    vm.myArrayOfDates = [];
+    $scope.$watch('myArrayOfDates', function (newValue, oldValue) {
+        if (newValue) {
+            console.log('my array changed, new size : ' + newValue.length);
+            console.log('myArrayOfDates', newValue);
+        }
+    }, true);
     
 
 
